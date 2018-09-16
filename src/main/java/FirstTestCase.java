2a0e86042714130a6c28f251b373a822ccb43c9b
Pablo <prenero4bc@gmail.com>
2018-09-16 13:28:22 +0200

    Counting number of links in a google search

--- src/main/java/FirstTestCase.java
+++ src/main/java/FirstTestCase.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
@@ -24,18 +25,27 @@ public class FirstTestCase {
         // Create a new instance of the Firefox driver
         WebDriver driver = new ChromeDriver();
 
+        // Maximize the window and wait until the browser is fully loaded
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(config.getString("timeoutKey")), TimeUnit.SECONDS);
 
-        //Launch the Online Store Website
+        // Search the url stored in .properties file
         driver.get(config.getString("urlKey"));
         // Print a Log In message to the screen
         System.out.println("Successfully opened the website" + config.getString("urlKey"));
 
+        // Selecting the html element whose id is "lst-ib" then search for a word and close the browser
         WebElement element = driver.findElement(By.id("lst-ib"));
         element.sendKeys(config.getString("wordKey"));
         element.submit();
 
-        driver.quit();
+        // Looking for the search results in the first page
+
+        List<WebElement> results = driver.findElements(By.className("rc"));
+
+        System.out.println("Count of links: " + results.size());
+
+        // closing the driver
+        //driver.quit();
     }
 }
